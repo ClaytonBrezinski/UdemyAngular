ffa783f545d063faa7a96cfb16f60d821326add9
OriginalMidas <clayton.brez@gmail.com>
2019-08-31 11:29:40 -0600

    push to work on at home

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  username = '';
+
+  updateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  eraseUsername() {
+    if (!this.username || this.username.length === 0) {
+      this.username = '';
+    }
+  }
 }
